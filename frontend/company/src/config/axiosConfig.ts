a3c0d4d670e3dba8c8cff014b311bf4ed1f4a97d
Cong Thanh <pdcthanh112.dev@gmail.com>
2022-12-30 18:45:28 +0700

    30/12/2022

---
+++ frontend/company/src/config/axiosConfig.ts
@@ -0,0 +1,26 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { store } from "../redux/store";
+
+const axiosConfig = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosConfig.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    if (config.headers) {
+      if (!config.headers.Authorization) {
+        const token = store.getState().auth.login.currentUser?.token;
+        if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+        }
+      }
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosConfig;
---
+++ frontend/company/src/config/index.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const { NODE_ENV, REACT_APP_API_URL, LOG_DIR } = process.env;
