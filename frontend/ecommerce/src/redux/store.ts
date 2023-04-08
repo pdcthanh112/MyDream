63eba422310b7569d0510877033322591e88fe71
pdcthanh <pdcthanh112.dev@gmail.com>
2023-04-08 10:29:57 +0700

    18/03/2023

--- frontend/ecommerce/src/redux/store.ts
+++ frontend/ecommerce/src/redux/store.ts
@@ -39,3 +39,7 @@ export const store = configureStore({
 });
 
 export let persistor = persistStore(store)
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
