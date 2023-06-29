20fbac1c12561cdd80383f80c148a262511993eb
pdcthanh <pdcthanh112.dev@gmail.com>
2023-06-29 20:16:16 +0700

    fix layout

--- frontend/ecommerce/src/app/layout.tsx
+++ frontend/ecommerce/src/app/layout.tsx
@@ -20,7 +20,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export function Layout({ Component, pageProps, router }: AppProps) {
+export default function Layout({ Component, pageProps, router }: AppProps) {
   const Header = styled(React.Fragment)`
     background-image: linear-gradient(to right, rgb(4, 171, 255), rgb(171, 235, 255));
   `;
