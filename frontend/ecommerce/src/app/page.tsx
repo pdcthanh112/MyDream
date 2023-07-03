0c6dd86a50fbe5cba010b71b575762d8c49d5e6f
pdcthanh <pdcthanh112.dev@gmail.com>
2023-07-03 10:14:38 +0700

    fix cc

--- frontend/ecommerce/src/app/page.tsx
+++ frontend/ecommerce/src/app/page.tsx
@@ -1,28 +1,4 @@
-// import type { AppProps } from 'next/app';
-// import Layout from './layout';
-// import { NextComponentType, NextPage, NextPageContext } from 'next';
-// import { ReactElement, ReactNode } from 'react';
-
-// type NextPageWithLayout = NextPage & {
-//   getLayout?: (page: ReactElement) => ReactNode
-//   Component?: NextComponentType<NextPageContext, any, any>
-// }
-
-// type AppPropsWithLayout = AppProps & {
-//   Component: NextPageWithLayout
-// }
-
-// export default function App({ Component, pageProps }: AppPropsWithLayout) {
-//   // return <Layout Component={Component} pageProps={pageProps} router={router} />;
-//   // const getLayout = Component.getLayout ?? (page => page);
-//   return (
-//     <Layout>
-//       <Component {...pageProps} />
-//     </Layout>
-//   );
-// }
-
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import Layout from './layout';
 import { NextComponentType, NextPage, NextPageContext } from 'next';
 import { ReactElement, ReactNode } from 'react';
@@ -35,12 +11,12 @@ type AppPropsWithLayout = AppProps & {
   Component: NextComponentType<NextPageContext, any, any> & NextPageWithLayout;
 };
 
-const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // return <Layout Component={Component} pageProps={pageProps} router={router} />;
+  // const getLayout = Component.getLayout ?? (page => page);
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
   );
-};
-
-export default App;
+}
--- frontend/ecommerce/src/pages/_app.tsx
+++ frontend/ecommerce/src/pages/_app.tsx
@@ -1,45 +1,3 @@
-// import React from 'react';
-// import '../app/globals.css';
-// import App from 'app/page';
-// import type { AppProps } from 'next/app';
-// import { Provider } from 'react-redux';
-// import { store, persistor } from '@redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ConfirmProvider } from 'material-ui-confirm';
-// import 'react-loading-skeleton/dist/skeleton.css';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// import { getAppData } from '@apis/appApi';
-// import { setAppData } from '@redux/features/appDataSlice';
-
-// export default function MyApp({ Component, pageProps, router }: AppProps) {
-//   const queryClient = new QueryClient();
-
-//   return (
-//     <GoogleOAuthProvider clientId="1085433653419-r6fptbnccc52h3q0rnhhsi5ge1onectp.apps.googleusercontent.com">
-//       <React.StrictMode>
-//         <Provider store={store}>
-//           <PersistGate loading={null} persistor={persistor}>
-//             <ConfirmProvider>
-//               <QueryClientProvider client={queryClient}>
-//                 <App Component={Component} pageProps={pageProps} router={router} />
-//               </QueryClientProvider>
-//             </ConfirmProvider>
-//           </PersistGate>
-//         </Provider>
-//       </React.StrictMode>
-//     </GoogleOAuthProvider>
-//   );
-// }
-
-// export const getStaticProps = async () => {
-//   await getAppData().then((response) => {
-//     if (response) {
-//       store.dispatch(setAppData(response));
-//     }
-//   });
-// };
-
 import React from 'react';
 import '../app/globals.css';
 import App from 'app/page';
@@ -54,9 +12,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { getAppData } from '@apis/appApi';
 import { setAppData } from '@redux/features/appDataSlice';
 
-const queryClient = new QueryClient();
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  const queryClient = new QueryClient();
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <GoogleOAuthProvider clientId="1085433653419-r6fptbnccc52h3q0rnhhsi5ge1onectp.apps.googleusercontent.com">
       <React.StrictMode>
@@ -72,9 +30,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
       </React.StrictMode>
     </GoogleOAuthProvider>
   );
-};
-
-export default MyApp;
+}
 
 export const getStaticProps = async () => {
   await getAppData().then((response) => {
@@ -83,3 +39,5 @@ export const getStaticProps = async () => {
     }
   });
 };
+
+
