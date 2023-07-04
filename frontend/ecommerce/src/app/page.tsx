d9815b56232a8897e99058b834e9e1198418251e
pdcthanh <pdcthanh112.dev@gmail.com>
2023-07-04 18:27:27 +0700

    fix

--- frontend/ecommerce/src/app/page.tsx
+++ frontend/ecommerce/src/app/page.tsx
@@ -2,9 +2,7 @@ import type { AppProps } from 'next/app';
 import RootLayout from './layout';
 import { NextComponentType, NextPage, NextPageContext } from 'next';
 import { ReactElement, ReactNode } from 'react';
-import { getAppData } from '@apis/appApi';
-import { setAppData } from '@redux/features/appDataSlice';
-import { store, persistor } from '@redux/store';
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -17,11 +15,3 @@ export default function App({ Component, pageProps }: AppPropsWithLayout): React
   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
   return <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>;
 }
-
-export const getStaticProps = async () => {
-  await getAppData().then((response) => {
-    if (response) {
-      store.dispatch(setAppData(response));
-    }
-  });
-};
