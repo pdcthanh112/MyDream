c704227d06ef4aa88fc22e45b9649b401d15fda4
pdcthanh <pdcthanh112.dev@gmail.com>
2023-07-04 17:17:33 +0700

    fix

--- frontend/ecommerce/src/app/page.tsx
+++ frontend/ecommerce/src/app/page.tsx
@@ -1,17 +1,36 @@
-import type { AppProps } from 'next/app';
-import RootLayout from './layout';
-import { NextComponentType, NextPage, NextPageContext } from 'next';
-import { ReactElement, ReactNode } from 'react';
+// import type { AppProps } from 'next/app';
+// import RootLayout from './layout';
+// import { NextComponentType, NextPage, NextPageContext } from 'next';
+// import { ReactElement, ReactNode } from 'react';
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+// export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+//   getLayout?: (page: ReactElement) => ReactNode;
+// };
+
+// export type AppPropsWithLayout = AppProps & {
+//   Component: NextComponentType<NextPageContext, any, any> & NextPageWithLayout;
+// };
+
+// export default function App({ Component, pageProps }: AppPropsWithLayout): ReactElement {
+//   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
+//   return <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>;
+// }
+import React, { ReactElement, ReactNode } from 'react';
+import { AppProps } from 'next/app';
+import RootLayout from './layout';
+import { NextComponentType, NextPageContext } from 'next';
+
+export type NextPageWithLayout<P = {}, IP = P> = NextComponentType<NextPageContext, any, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
 export type AppPropsWithLayout = AppProps & {
-  Component: NextComponentType<NextPageContext, any, any> & NextPageWithLayout;
+  Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout): ReactElement {
+function App({ Component, pageProps }: AppPropsWithLayout): ReactElement {
   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
   return <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>;
 }
+
+export default App;
