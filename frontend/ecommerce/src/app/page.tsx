c4dff9c261706a198d10edf5c9d83ff9d14947fe
pdcthanh <pdcthanh112.dev@gmail.com>
2023-07-05 13:44:32 +0700

    fix

--- frontend/ecommerce/src/app/page.tsx
+++ frontend/ecommerce/src/app/page.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app';
 import { NextComponentType, NextPage, NextPageContext } from 'next';
 import { ReactElement, ReactNode } from 'react';
+import RootLayout from './layout';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,7 +12,7 @@ export type AppPropsWithLayout = AppProps & {
   Component: NextComponentType<NextPageContext, any, any> & NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout): ReactElement {
+export default function App({ Component, pageProps, router }: AppPropsWithLayout): ReactElement {
   const getLayout = Component.getLayout ?? ((page: React.ReactNode) => page);
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+  return <RootLayout>{getLayout(<Component {...pageProps} {...router} />)}</RootLayout>;
 }
